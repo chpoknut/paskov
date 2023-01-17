dccd1bf98fc9946cf9a0ee5892fa1e6f6d6bc496
Valik <91689953+chpoknut@users.noreply.github.com>
2023-01-17 11:09:47 +0200

    Create alegori.py

---
+++ alegori.py
@@ -0,0 +1,45 @@
+def func1(arg1, arg2):
+    var7 = func2(arg1, arg2)
+    var12 = func3(arg1, arg2)
+    var13 = -39658939 + (-1284842318 - (arg2 ^ arg1))
+    var14 = var13 - ((arg2 & arg1) | var7)
+    var15 = var14 | var12
+    if arg1 < arg1:
+        var16 = var15 + 1580946948 + var13 | var7
+    else:
+        var16 = var14 | (283 - var12 + var7)
+    var17 = arg1 + var7 & var14 + arg1
+    var18 = arg2 - var15 + var17 | var13
+    var19 = (-938855996 - var18) - (var14 | -726)
+    if var7 < var7:
+        var20 = (var18 + var17) | var12
+    else:
+        var20 = (var18 - 984) | var15 ^ -838682750
+    var21 = arg1 - var12
+    var22 = var13 + ((-2018648753 & arg2) ^ var14)
+    var23 = (var21 - var19 + var13) | 1339670495
+    var24 = var22 | (var18 ^ var23) & var22
+    var25 = var14 + 584066803
+    var26 = var14 ^ var25 ^ (var24 + var21)
+    var27 = 794699691 & var23 ^ var23 ^ var23
+    var28 = var15 & var22
+    result = (((var18 | arg1) - var28) | var21 & var17 - var23) | var22 | arg1 | var12 | var27 - arg2 ^ var13
+    return result
+def func3(arg8, arg9):
+    var10 = 0
+    for var11 in range(18):
+        var10 += arg9 + 0
+    return var10
+def func2(arg3, arg4):
+    var5 = 0
+    for var6 in ((((8 | arg4 | ((((arg4 ^ arg3) - arg3) | var5) & var5 ^ 7) + arg4) ^ arg4) - arg4) ^ -7 for i in ((i - arg3) & -4 for i in (var5 + 1 for i in xrange(25)))):
+        var5 += -3 ^ (-8 | arg4)
+    return var5
+if __name__ == "__main__":
+    print 'prog_size: 5'
+    print 'func_number: 4'
+    print 'arg_number: 29'
+    for i in xrange(25000):
+        x = 5
+        x = func1(x, i)
+        print x,
